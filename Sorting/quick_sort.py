c3074b35a685ee2970c03c018cd6f579a35c8da6
Siddhant Kedar <96174232+SiddhantKedar@users.noreply.github.com>
2024-03-07 09:32:23 +0530

    Create quick_sort.py

---
+++ Sorting/quick_sort.py
@@ -0,0 +1,26 @@
+count = 0
+
+def partition(array, left, right):
+    smaller_index = left - 1
+    pivot = array[right]
+    for i in range(left, right):
+        global count
+        count += 1
+        if array[i] < pivot:
+            smaller_index += 1
+            array[smaller_index], array[i] = array[i], array[smaller_index]
+    array[smaller_index+1], array[right] = array[right], array[smaller_index+1]
+    print(array)
+    return (smaller_index+1)
+
+def quick_sort(array, left, right):
+    if left < right:
+        partitioning_index = partition(array, left, right)
+        print(partitioning_index)
+        quick_sort(array,left,partitioning_index-1)
+        quick_sort(array,partitioning_index+1,right)
+
+array = [5,9,3,10,45,2,0]
+quick_sort(array, 0, (len(array)-1))
+print(array)
+print(f'Number of comparisons = {count}')
